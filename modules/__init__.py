a8154170727aa9f96cee7d98ac4bb85cf6fc715c
ChrisMoon90 <chrismoon90@gmail.com>
2021-08-17 11:02:24 -0700

    Classes Added & Thread Start-Stop Working

--- modules/__init__.py
+++ modules/__init__.py
@@ -9,15 +9,26 @@ from modules.sensors import *
 
 
 def get_db_indexes(): 
-    indexes = {'s0':0, 's1':1, 's2':2}
-    print(indexes)
+    filename = "./logs/indexes.log"
+    if os.path.exists(filename):
+        with open(filename) as f:
+            indexes_raw = f.readlines()
+            indexes = eval(indexes_raw[0])
+    else:
+        print("Index file does not exist. Index file will be created.")
+        indexes = {'s0':0, 's1':1, 's2':2}
+        with open(filename, 'w') as f:
+                f.write(str(indexes))
+    print("db_indexes: ", indexes)
     return indexes
 
 @socketio.on('connect')
 def MBC_connect():
     print('Client Connected')
     socketio.emit('connected',  {'connected': 'Connection Request Accepted'})
+    indexes = get_db_indexes()
     socketio.emit('indexes', indexes)
+    socketio.emit('logState', b.running)
    
 @socketio.on('disconnect')
 def MBC_disconnect():
@@ -25,17 +36,32 @@ def MBC_disconnect():
 
 @socketio.on('index_change')
 def index_change(indexes_in):
-    global indexes
     indexes = indexes_in
+    filename = "./logs/indexes.log"
+    with open(filename, 'w') as f:
+        f.truncate()
+        f.write(str(indexes))
     socketio.emit('indexes', indexes)
     print("Indexes Received from Client & Broadcasted: %s" % indexes)
     
+@socketio.on('toggle_logState')
+def toggle_logState(logState_in):
+    logState = not logState_in
+    b.set_run_state(logState)
+    if logState == True:
+        thread2 = socketio.start_background_task(target=b.save_to_file, sleep = 15)
+    else:
+        print("Stopping Logging Thread") 
+    socketio.emit('logState', logState)
+
 @socketio.on('delete_log')
-def delete_log():
-    print("Deleting Temp.log File")
+def delete_log():   
     filename = "./logs/Temps.log"
     if os.path.exists(filename):
       os.remove(filename)
+      msg = "Temp Log File Successfully Deleted"
+      print(msg)
+      socketio.emit('log_deleted', msg)
     else:
       print("The file does not exist")
        
@@ -45,9 +71,10 @@ def index0():
         return redirect('/ui')
     except ValueError:
             return str(e)
-   
-print("Starting Threads")
-thread = socketio.start_background_task(target=RTD_Temp, sleep = 0)
-thread2 = socketio.start_background_task(target=save_to_file, sleep = 15)
 
-indexes = get_db_indexes()
+
+print("Starting Temp Thread")
+a = tempAPI()
+thread1 = socketio.start_background_task(target=a.RTD_Temp, sleep = 2)
+
+b = logAPI(a)
--- modules/sensors/__init__.py
+++ modules/sensors/__init__.py
@@ -10,33 +10,48 @@ device_list = sensor_i2c.get_devices()
 active_i2c_devs = sensor_i2c.get_i2c_list(device_list)
 print("Active I2C Devices: %s" % active_i2c_devs)
 
-temps = {'time': None, 0: None, 1: None, 2: None}
-def RTD_Temp(sleep):
-    print("Starting RTD Temp Background Process")
-    global temps
-    while True:        
-        temp_time = time.strftime("%H:%M:%S", time.localtime())
-        temps['time'] = temp_time
-        num_sensors = len(active_i2c_devs)
-        for i in range(0, num_sensors):
-            i2c_addr = active_i2c_devs[i]
-            cur_temp = sensor_i2c.get_reading(device_list,i2c_addr)           
-            temps[i] = cur_temp
-        print("Temp Output: %s" % temps)      
-        emit_temp(temps)
-        socketio.sleep(sleep)
-                     
-def emit_temp(temps):
-    socketio.emit('newtemps', temps)
 
-def save_to_file(sleep):
-    print("Starting save_to_file")
-    socketio.sleep(5)
-    while True:
-        filename = "./logs/Temps.log"
-        formatted_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-        msg = "%s, %s\n" % (formatted_time, temps)
-        print("Saving to File: %s" %temps)
-        with open(filename, "a") as file:
-            file.write(msg)
-        socketio.sleep(sleep)
+class tempAPI:   
+    def __init__(self):
+        self.temps = {'time': None, 0: None, 1: None, 2: None}
+
+    def RTD_Temp(self, sleep):
+        print("Starting RTD Temp Background Process")
+        #global temps
+        while True:        
+            temp_time = time.strftime("%H:%M:%S", time.localtime())
+            self.temps['time'] = temp_time
+            num_sensors = len(active_i2c_devs)
+            for i in range(0, num_sensors):
+                i2c_addr = active_i2c_devs[i]
+                cur_temp = sensor_i2c.get_reading(device_list,i2c_addr)           
+                self.temps[i] = cur_temp
+            print("Temp Output: %s" % self.temps)      
+            self.emit_temp()
+            socketio.sleep(sleep)
+                        
+    def emit_temp(self):
+        socketio.emit('newtemps', self.temps)
+
+
+class logAPI:
+    def __init__(self, a):
+        self.running = False
+        self.temps = a.temps
+
+    def set_run_state(self, logState):
+        self.running = logState
+        print("Logging run_state changed: ", self.running)
+
+    def save_to_file(self, sleep):
+        print("Starting Logging")
+        #socketio.sleep(2)
+        while self.running:
+            filename = "./logs/Temps.log"
+            formatted_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+            msg = "%s, %s\n" % (formatted_time, self.temps)
+            print("Saving to File: %s" %self.temps)
+            with open(filename, "a") as file:
+                file.write(msg)
+            socketio.sleep(sleep)
+        print("Log Thread Terminated")
