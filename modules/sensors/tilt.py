72b2012655d3e42e9841f6beceac7fb296227d24
ChrisMoon90 <chrismoon90@gmail.com>
2023-07-31 22:52:15 -0700

    Tilt Update

--- modules/sensors/tilt.py
+++ modules/sensors/tilt.py
@@ -9,6 +9,41 @@ from bleak import BleakScanner
 from modules.sensors.__init__ import SensorBase
 from modules.app_config import socketio, cache
 
+class Tilt(SensorBase):
+    def __init__(self, uuid):
+        self.uuid = uuid
+        self.t_cache = {'temp': 0, 'sg': 0, 'txpower': 0, 'rssi': 0}
+        self.dev_name = SensorBase.sensor_type(SensorBase, 'SG')
+        self.s_num = SensorBase.s_count['Total'] - 1
+        cache['INIT'].append({'function': self.run_tilt, 'sleep': 2}) #, 'dev_id': self.uuid, 's_num': self.s_num})
+        cache["SENSORS"][self.s_num] = {'com_type': 'ble', 'dev_name': self.dev_name, 'dev_id': self.uuid, 'cur_read': "{0:.3f}".format(0)}
+
+    def get_t_cache(self):
+        return self.t_cache
+
+    def run_tilt(self, sleep):
+        print("Starting TILT Background Process as Sensor %s" % self.s_num)  
+        while True: 
+            print('tilt_run...')     
+            try:
+                # DO SOMETHING TO GET ADVERTISING DATA FOR SPECIFIC UUID
+                advertisement_data = ""
+                ad_data = advertisement_data.manufacturer_data[0x004C]
+                ibeacon = ibeacon_format.parse(ad_data)
+                rssi = advertisement_data.rssi
+                self.t_cache = {'temp': ibeacon.major, 'sg': float(ibeacon.minor)/1000, 'txpower': ibeacon.power, 'rssi': rssi}
+                cache['SENSORS'][self.s_num]['cur_read'] = self.t_cache['sg']
+                print(self.uuid, self.t_cache)
+            except KeyError:
+                pass
+            except ConstError:
+                pass
+            except:
+                print('Other run_tilt Error')
+            socketio.sleep(sleep)
+
+
+a_tilts = []
 tilts = {
     'a495bb10-c5b1-4b44-b512-1370f02d74de': 'Red',
     'a495bb20-c5b1-4b44-b512-1370f02d74de': 'Green',
@@ -38,58 +73,39 @@ ibeacon_format = Struct(
     "power" / Int8sl,
 )
 
-class Tilt(SensorBase):
-    a_tilts = {}
+async def tilt_init():
+    scanner = BleakScanner(device_found)
+    await scanner.start()
+    await asyncio.sleep(2)
+    await scanner.stop()
+    
+def device_found(device, advertisement_data):
+    print('in device found')   
+    try:
+        ad_data = advertisement_data.manufacturer_data[0x004C]
+        ibeacon = ibeacon_format.parse(ad_data)
+        uuid = str(UUID(bytes=bytes(ibeacon.uuid)))
+        for key in tilts: 
+            if uuid == key:
+                print('to unique')
+                if unique(uuid):
+                    print('ceating tilt dev')
+                    dev = Tilt(uuid) 
+                    print(dev)                      
+    except KeyError:
+        pass
+    except ConstError:
+        pass
+    except:
+        print('Other device_found Error')
 
-    def get_tilt_cache():
-        return Tilt.a_tilts
-
-    def unique(uuid):
-        if uuid not in Tilt.a_tilts.keys():
-            Tilt.a_tilts[uuid] = {'temp': 0, 'sg': 0, 'txpower': 0, 'rssi': 0}
-            print('Tilt Added: ', uuid)
-            return True
-        else:
-            return False
-
-    async def device_found(device, advertisement_data):
-        try:
-            ad_data = advertisement_data.manufacturer_data[0x004C]
-            ibeacon = ibeacon_format.parse(ad_data)
-            uuid = str(UUID(bytes=bytes(ibeacon.uuid)))
-            for key in tilts: 
-                if uuid == key:
-                    if Tilt.unique(uuid):
-                        type = 'SG'
-                        print('go to sensor type')
-                        dev_name = SensorBase.sensor_type(SensorBase, type)
-                        s_num = SensorBase.s_count['Total'] - 1
-                        print('s_num: ', s_num)
-                        cache["SENSORS"][s_num] = {'com_type': 'ble', 'dev_name': dev_name, 'dev_id': uuid, 'cur_read': "{0:.3f}".format(0)}
-                    rssi = advertisement_data.rssi
-                    Tilt.a_tilts[uuid] = {'temp': ibeacon.major, 'sg': float(ibeacon.minor)/1000, 'txpower': ibeacon.power, 'rssi': rssi}
-                    for key, val in cache['SENSORS'].items():
-                        print(key, val)
-                        if val['dev_id'] == uuid:
-                            s_num = key
-                    cache['SENSORS'][s_num]['cur_read'] = Tilt.a_tilts[uuid]['sg']
-        except KeyError:
-            pass
-        except ConstError:
-            pass
-        except:
-            print('Other device_found Error')
+def unique(uuid):
+    if uuid not in a_tilts:
+        a_tilts.append(uuid)
+        print('Tilt Added: ', uuid)
+        return True
+    else:
+        return False
 
 
-async def tilt_init(sleep):
-    scanner = BleakScanner(Tilt.device_found) #add filter here later
-    while True:
-        await scanner.start()
-        await asyncio.sleep(sleep)
-        await scanner.stop()
-
-
-cache["INIT"].append({"function": tilt_init, "sleep": 5})
-
-# task = socketio.start_background_task(tilt_init, 123)
-# asyncio.run(init())
+asyncio.run(tilt_init())
